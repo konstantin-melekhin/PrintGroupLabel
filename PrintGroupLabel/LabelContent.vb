14d9374b1a1658bdce325dde93c347321331336a
Konstantin Melekhin <kostya_mel@mail.ru>
2020-07-27 21:00:32 +0200

    Добавьте файлы проекта.

---
+++ PrintGroupLabel/LabelContent.vb
@@ -0,0 +1,128 @@
+﻿Module LabelContent
+    Public LabelSNContent As String
+
+    Public Function LabelSN(LabelDate As String, PrintTextSN As String, PrintCodeSN As String, Model As String, labelCount As Integer)
+        '^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR10,10~SD15^JUS^LRN^CI0^XZ
+        LabelSNContent = "
+^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR10,10~MD5^JUS^LRN^CI0^XZ
+^XA
+^MMT
+^PW650
+^LL0201
+^LS0
+^FO96,128^GFA,02560,02560,00040,:Z64:
+eJztkz2unTAQhX1F4dJlujdLyAYiO0tKeQsEXN2CkiW8nbwYUVBmC0YU73VxOYXlyTE/bwORkoYpGBkdPp0zY5S66qqr/rZa6aUKlm6eFIn/8p2S0qN4ZXUv8X6L2seiExnkGRoaggTohEk6vQgrS4Mwm2w877xfMkJHo8SiS0U3bjrwEmV96gZZQ3a0FtQkGbpq4znouJVDBwP9tGQiHRLpMTakPVqtrEW71zjioFQVzXNdM72ZkGmYkGjweuGsbDILw+DgOW080y/Q1drj2znQJ69GS2xmzwevX9+zy6aD8zU4GuAvWuVYr1HY9IF59yfTeybM6Rt4nra8RRfR2qjn5fQn0+83SoU3zOCV+bEVx/AH3ixHXiMPeSlzqg+eKrFbl9Ak6mnXgZerbJ01vqG+5M2q+GttA3/I+zx5uquSLTHBKzpVeHXRYX5suh/x4G261z4kN0ycqQOvzM+W+WXXqSOv6W7phQh8wWLTxpOE/RLm14pS5/y6iq0zmy5gyXi37/fnM7LL6uTBX7Sw14YGj7jzInivj3jHbVTxk1e2Gr52bLzx8If71ylr8gNxP8BL/+yvueqqq/5n/QGBgl71:2AB9
+^FO64,128^GFA,00256,00256,00004,:Z64:
+eJxjYKAEsP9gYOD/AMUPGBjkDzAw2ANxPYhuANJA/J+BgRGIGf4B6T8MDMxAzA6igVrZoVrlkbU3QLSDtNZDtf2nyJF4AQCESRoM:9F12
+^FO0,128^GFA,00768,00768,00012,:Z64:
+eJxjYBjZgP2AHOMBKNv+hz3zAyi7/g+C/eOPfTOM/aFE/iBM/Q8G+8cw8f8M9p/h7AcIdv0P+Y9w8//Z/YeJM/6xq4exGezs7KFsxgIbGXmoesYfCHGGvzV2/HDz/yDY9R/k2GHm6z+wg7t5FBAHAOLPMJc=:B706
+^FO384,0^GFA,00768,00768,00012,:Z64:
+eJztjjEKAjEQRSeskHKOMBeRzbVSLLiQwjJH8QoDW2zpFQJ7AC1j4zfZuGAjdmLhL8Ln8XkZon++HofXPn7ke6HBwly59F4CIuKdx8otZgikco6T5vKunIL6Hbf9yCmTq7ZB6KDerL8MHJAyo3E7qZc6pywxpOYxwHxU30vxd8Cl7B2e/Fb2Z175shTPqXI1up3cpS5t/R3/hTwAHyFPIw==:0A21
+^BY5,3,81^FT41,137^BCN,,N,N
+^FD>;" & PrintCodeSN & "^FS
+^FT30,51^A0N,33,33^FH\^FD" & Model & "^FS
+^FT395,169^A0N,33,33^FH\^FD" & PrintTextSN & "^FS
+^FT471,51^A0N,33,33^FH\^FD" & LabelDate & "^FS
+^PQ" & labelCount & ",0,1,Y^XZ"
+
+        Return (LabelSNContent)
+    End Function
+
+    Public Function LabelSN4(LabelDate As String, PrintTextSN As String, PrintCodeSN As String, Model As String, labelCount As Integer)
+        '^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR10,10~SD15^JUS^LRN^CI0^XZ
+        LabelSNContent = "
+^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,10^JMA^MD5^JUS^LRN^CI0^XZ
+^XA
+^MMT
+^PW650
+^LL0201
+^LS0
+^BY5,3,81^FT41,137^BCN,,N,N
+^FD>;" & PrintTextSN & "^FS
+^FT30,51^A0N,33,33^FH\^FD" & Model & "^FS
+^FT471,51^A0N,33,33^FH\^FD" & LabelDate & "^FS
+^FT163,169^A0N,33,33^FH\^FDSN:^FS
+^FT215,169^A0N,33,33^FH\^FD" & PrintTextSN & "^FS
+^PQ" & labelCount & ",0,1,Y^XZ"
+
+        Return (LabelSNContent)
+    End Function
+
+    Public Function LabelSN3(LabelDate As String, PrintTextSN As String, PrintCodeSN As String, Model As String, labelCount As Integer)
+        '^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR10,10~SD15^JUS^LRN^CI0^XZ
+        LabelSNContent = "
+^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,10^JMA^MD5^JUS^LRN^CI0^XZ
+^XA
+^MMT
+^PW650
+^LL0201
+^LS0
+^BY5,3,87^FT48,143^BCN,,N,N
+^FD>;" & PrintTextSN & "^FS
+^FT47,48^A0N,33,33^FH\^FD" & Model & "^FS
+^FT459,48^A0N,33,33^FH\^FD" & LabelDate & "^FS
+^FT163,177^A0N,37,36^FH\^FDSN:^FS
+^FT215,177^A0N,37,36^FH\^FD" & PrintTextSN & "^FS
+^PQ" & labelCount & ",0,1,Y^XZ"
+
+        Return (LabelSNContent)
+    End Function
+
+
+
+
+    ' Описание операторов ZPL 
+    '^XA - начало этикетки
+    '^XZ - конец этикетки
+    '^MMT - определяет как будут печататься этикетки, групоой или одиночке
+    '^LS10 сдвиг влево всех элементов до начала отсчета
+    '^FT40,293^A0N,108,108^FH\^FDID^FS где
+    '^FT40,293 позиция FTx,y
+    '^A0N - поворот (Т = Normal),108,108 - высота шрифта, ширина текста
+
+
+    Public Function LabelSN2(LabelDate As String, PrintTextSN As String, Model As String, labelCount As Integer)
+        '^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR10,10~SD15^JUS^LRN^CI0^XZ
+        LabelSNContent = "
+^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,20^JMA^MD5^JUS^LRN^CI0^XZ
+^XA
+^MMT
+^PW650
+^LL0201
+^LS0
+^BY5,3,96^FT48,143^BCN,,N,N
+^FD>;" & PrintTextSN & "^FS
+^FT51,43^A0N,37,36^FH\^FD" & Model & "^FS
+^FT445,43^A0N,37,36^FH\^FD" & LabelDate & "^FS
+^FT153,182^A0N,42,40^FH\^FDSN:^FS
+^FT215,182^A0N,42,40^FH\^FD" & PrintTextSN & "^FS
+^PQ" & labelCount & ",0,1,Y^XZ"
+        Return (LabelSNContent)
+    End Function
+
+
+    Public Function LabelPallet(LabelDate As String, Liter As String, Pallet As String, Box As Integer)
+        '^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR10,10~SD15^JUS^LRN^CI0^XZ
+        LabelSNContent = "
+^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,20^JMA^MD5^JUS^LRN^CI0^XZ
+^XA
+^MMT
+^PW650
+^LL0201
+^LS0
+^FO0,96^GFA,03840,03840,00040,:Z64:
+eJztVDGO2zAQlEEcWOoJ+snxKXkKeVCh0k9IfhIFKVzeFwSkcEoWBkLfbTgZkhKNk4UkQIKk0boYyx6NZneWapq99tprr7322utflJ7eXlunwwbtAf7tD3DtFq/DWm/SssGzax6mzecC4+pGv8U7AG51Y6C/O+ZhTWusaGlxx7vzDNHBLHc/Eh8TqJev93rB0vRB+IHX3+ESAI0Z3+mgQ1cGidgG207NgagQ1BVjAvZhHHqa76aiBy2F19GnUGgqMNpPoJosehhC1JkXWkSVeTHRTZ94N73TVbSnPyMGIM8TyPP2RJ6p/nCc9UwwExRtEnRsvRmgEm/RO0tMetGOdsQQaZcgrdjnC79Xfx/Psx64Y7ZPY3J24EDMkdGj6r1HyHrIvIHxFJ5wqvqKJVIcyUtzQSTP9EFRzwwEpqReK49die2YByTpnYreadYDlqAxIOdb9GwfZn9J74PGa9XraTXz4Eofsz+mzpi/VX9P3MBYNtOUfhNkPa/wpeo9QR4uzTw/W+ZX2n5mzOc6P1daYh7TLQ8lBD4cn+v8nC083jgHm2JOV+ydS1L1mCvy/vku78sM3BeCqnqjFZt5nS+Ll4BdTWn/uJxVrwuF19JOWuSJkM4v91nHm97CY4c8GKqcD66Rox7bvvlr536VpIMW8nlrutjk81H1VrW8bPRl699bqa2X0k/0flVq6+X1J3p/m/eb/vb6j/UDsWP3lQ==:1CE3
+^FO96,0^GFA,02688,02688,00028,:Z64:
+eJztkkFOwzAQRe1m4aWP4BtwhV6BA3CXCPUAXClVF1lyBFxYdOuusNpRPvOdxiQSS0BCZBRlpLy8fNsTY9Zaa63vKA/In2fZTWyT9RZNc6lMnPjkz0AK6EIvptWWquejj0DcIobnQVkMsXohBXqtqi+w/MLc2yb1gByAR7Y4y2vV00uU7cnmHqXj4HLI2LHN8s5ITqw0mtzuH7iAT++I1OSGLG47fTR5ZO/0HPPI2ukwCjshqgcMyqJHnjFdXfVC12LhoeSNXoe36nF/ONHT15Xt4ZZez7yRHaSZvFDyevUauHvd30Hq/lC8J82zgxeei0z7s1yhh+O5jOfZs908ssFpnhnnsKtzKF4QeobzA+zMi+plr3mGc8dgca1eR1Y8/i8n1Y+TdzGcbcnbXEx4jebOym0Oy/ry4U+y9Lt5a631f+sD00RP8Q==:08B9
+^FO96,64^GFA,01792,01792,00028,:Z64:
+eJzt0jFuwyAUBmAQA1IXxo4+Qo/wepMeBZbOuRK5QY/gsSOjVT3x90eWbYiUTlWy+EWKEC+f//CwMWed9X8FoNxp2T96z3DLXWemYuL7vvFxLA2SFEcKVMdHcBlQZd5dgLKn7BUPnYBp3p2gbo5odPzl5hq6cdgcl6OTHDfH5eCWl4QEV52GJXIpuXPtG1ad+hITP7nLM2+rW+jAjdTNxeJwQJbczSVgz0PXW8+nsXexdzIfecwa8qI5HPf6vAXd+fI3XXe+eIWl83SSMZ6Pf+BCJ8xrox1dG7BVoWtXIreuro6XYIZ5TjUGpSu+3bsZ8sKPXJSuhPa+jHn+Sz6L1dfs+cqZI++sx9QvxfFAmw==:7B89
+^FT323,176^A0N,50,50^FH\^FD" & Box & "^FS
+^FT323,62^A0N,50,50^FH\^FD" & Liter & "^FS
+^FT323,117^A0N,50,50^FH\^FD" & Pallet & "^FS
+^FT470,44^A0N,33,33^FH\^FD" & LabelDate & "^FS
+^PQ1,0,1,Y^XZ"
+
+        Return (LabelSNContent)
+    End Function
+
+End Module
---
+++ PrintGroupLabel/WorkForm2.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class WorkForm2
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DG_SelectedBox = New System.Windows.Forms.DataGridView()
+        Me.Controllabel = New System.Windows.Forms.Label()
+        Me.TB_ScanSN = New System.Windows.Forms.TextBox()
+        Me.PrintSerialPort = New System.IO.Ports.SerialPort(Me.components)
+        Me.GroupBox5 = New System.Windows.Forms.GroupBox()
+        Me.CurrrentDateLabel = New System.Windows.Forms.Label()
+        Me.CurrrentTimeLabel = New System.Windows.Forms.Label()
+        Me.Label_WorkDate = New System.Windows.Forms.Label()
+        Me.CurrentTimeTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.GB_Lot = New System.Windows.Forms.GroupBox()
+        Me.DG_Lot = New System.Windows.Forms.DataGridView()
+        Me.NumBox = New System.Windows.Forms.NumericUpDown()
+        Me.CB_SN_or_Box = New System.Windows.Forms.CheckBox()
+        CType(Me.DG_SelectedBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox5.SuspendLayout()
+        Me.GB_Lot.SuspendLayout()
+        CType(Me.DG_Lot, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DG_SelectedBox
+        '
+        Me.DG_SelectedBox.AllowUserToAddRows = False
+        Me.DG_SelectedBox.AllowUserToDeleteRows = False
+        Me.DG_SelectedBox.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DG_SelectedBox.Location = New System.Drawing.Point(17, 236)
+        Me.DG_SelectedBox.Name = "DG_SelectedBox"
+        Me.DG_SelectedBox.ReadOnly = True
+        Me.DG_SelectedBox.Size = New System.Drawing.Size(760, 389)
+        Me.DG_SelectedBox.TabIndex = 49
+        '
+        'Controllabel
+        '
+        Me.Controllabel.AutoSize = True
+        Me.Controllabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Controllabel.Location = New System.Drawing.Point(12, 156)
+        Me.Controllabel.Name = "Controllabel"
+        Me.Controllabel.Size = New System.Drawing.Size(139, 25)
+        Me.Controllabel.TabIndex = 48
+        Me.Controllabel.Text = "Controllabel"
+        '
+        'TB_ScanSN
+        '
+        Me.TB_ScanSN.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.TB_ScanSN.Location = New System.Drawing.Point(12, 57)
+        Me.TB_ScanSN.Name = "TB_ScanSN"
+        Me.TB_ScanSN.Size = New System.Drawing.Size(366, 26)
+        Me.TB_ScanSN.TabIndex = 47
+        '
+        'GroupBox5
+        '
+        Me.GroupBox5.BackColor = System.Drawing.SystemColors.Control
+        Me.GroupBox5.Controls.Add(Me.CurrrentDateLabel)
+        Me.GroupBox5.Controls.Add(Me.CurrrentTimeLabel)
+        Me.GroupBox5.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.GroupBox5.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.GroupBox5.Location = New System.Drawing.Point(456, 23)
+        Me.GroupBox5.Name = "GroupBox5"
+        Me.GroupBox5.Size = New System.Drawing.Size(316, 128)
+        Me.GroupBox5.TabIndex = 50
+        Me.GroupBox5.TabStop = False
+        Me.GroupBox5.Text = "Дата и Время на этикетке СН"
+        '
+        'CurrrentDateLabel
+        '
+        Me.CurrrentDateLabel.AutoSize = True
+        Me.CurrrentDateLabel.BackColor = System.Drawing.SystemColors.Control
+        Me.CurrrentDateLabel.Font = New System.Drawing.Font("Arial", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.CurrrentDateLabel.Location = New System.Drawing.Point(6, 28)
+        Me.CurrrentDateLabel.Name = "CurrrentDateLabel"
+        Me.CurrrentDateLabel.Size = New System.Drawing.Size(241, 41)
+        Me.CurrrentDateLabel.TabIndex = 6
+        Me.CurrrentDateLabel.Text = "Current DATE"
+        '
+        'CurrrentTimeLabel
+        '
+        Me.CurrrentTimeLabel.AutoSize = True
+        Me.CurrrentTimeLabel.BackColor = System.Drawing.SystemColors.Control
+        Me.CurrrentTimeLabel.Font = New System.Drawing.Font("Arial", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.CurrrentTimeLabel.Location = New System.Drawing.Point(6, 77)
+        Me.CurrrentTimeLabel.Name = "CurrrentTimeLabel"
+        Me.CurrrentTimeLabel.Size = New System.Drawing.Size(234, 41)
+        Me.CurrrentTimeLabel.TabIndex = 6
+        Me.CurrrentTimeLabel.Text = "Current TIME"
+        '
+        'Label_WorkDate
+        '
+        Me.Label_WorkDate.AutoSize = True
+        Me.Label_WorkDate.BackColor = System.Drawing.SystemColors.Control
+        Me.Label_WorkDate.Font = New System.Drawing.Font("Arial", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label_WorkDate.Location = New System.Drawing.Point(778, 32)
+        Me.Label_WorkDate.Name = "Label_WorkDate"
+        Me.Label_WorkDate.Size = New System.Drawing.Size(203, 41)
+        Me.Label_WorkDate.TabIndex = 51
+        Me.Label_WorkDate.Text = "Work DATE"
+        Me.Label_WorkDate.Visible = False
+        '
+        'CurrentTimeTimer
+        '
+        Me.CurrentTimeTimer.Interval = 1000
+        '
+        'GB_Lot
+        '
+        Me.GB_Lot.Controls.Add(Me.DG_Lot)
+        Me.GB_Lot.Location = New System.Drawing.Point(827, 236)
+        Me.GB_Lot.Name = "GB_Lot"
+        Me.GB_Lot.Size = New System.Drawing.Size(544, 363)
+        Me.GB_Lot.TabIndex = 54
+        Me.GB_Lot.TabStop = False
+        Me.GB_Lot.Text = "Выберите лот"
+        '
+        'DG_Lot
+        '
+        Me.DG_Lot.AllowUserToAddRows = False
+        Me.DG_Lot.AllowUserToDeleteRows = False
+        Me.DG_Lot.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DG_Lot.Location = New System.Drawing.Point(6, 19)
+        Me.DG_Lot.Name = "DG_Lot"
+        Me.DG_Lot.ReadOnly = True
+        Me.DG_Lot.Size = New System.Drawing.Size(532, 338)
+        Me.DG_Lot.TabIndex = 0
+        '
+        'NumBox
+        '
+        Me.NumBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.NumBox.Location = New System.Drawing.Point(12, 114)
+        Me.NumBox.Maximum = New Decimal(New Integer() {10000, 0, 0, 0})
+        Me.NumBox.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumBox.Name = "NumBox"
+        Me.NumBox.Size = New System.Drawing.Size(183, 26)
+        Me.NumBox.TabIndex = 53
+        Me.NumBox.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'CB_SN_or_Box
+        '
+        Me.CB_SN_or_Box.AutoSize = True
+        Me.CB_SN_or_Box.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.CB_SN_or_Box.Location = New System.Drawing.Point(12, 23)
+        Me.CB_SN_or_Box.Name = "CB_SN_or_Box"
+        Me.CB_SN_or_Box.Size = New System.Drawing.Size(290, 24)
+        Me.CB_SN_or_Box.TabIndex = 52
+        Me.CB_SN_or_Box.Text = "Отсканируйте серийный номер"
+        Me.CB_SN_or_Box.UseVisualStyleBackColor = True
+        '
+        'WorkForm2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1403, 778)
+        Me.Controls.Add(Me.DG_SelectedBox)
+        Me.Controls.Add(Me.Controllabel)
+        Me.Controls.Add(Me.TB_ScanSN)
+        Me.Controls.Add(Me.GroupBox5)
+        Me.Controls.Add(Me.Label_WorkDate)
+        Me.Controls.Add(Me.GB_Lot)
+        Me.Controls.Add(Me.NumBox)
+        Me.Controls.Add(Me.CB_SN_or_Box)
+        Me.Name = "WorkForm2"
+        Me.Text = "WorkForm2"
+        CType(Me.DG_SelectedBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox5.ResumeLayout(False)
+        Me.GroupBox5.PerformLayout()
+        Me.GB_Lot.ResumeLayout(False)
+        CType(Me.DG_Lot, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DG_SelectedBox As DataGridView
+    Friend WithEvents Controllabel As Label
+    Friend WithEvents TB_ScanSN As TextBox
+    Friend WithEvents PrintSerialPort As IO.Ports.SerialPort
+    Friend WithEvents GroupBox5 As GroupBox
+    Friend WithEvents CurrrentDateLabel As Label
+    Friend WithEvents CurrrentTimeLabel As Label
+    Friend WithEvents Label_WorkDate As Label
+    Friend WithEvents CurrentTimeTimer As Timer
+    Friend WithEvents GB_Lot As GroupBox
+    Friend WithEvents DG_Lot As DataGridView
+    Friend WithEvents NumBox As NumericUpDown
+    Friend WithEvents CB_SN_or_Box As CheckBox
+End Class
